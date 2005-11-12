7469d4026102dee3e546d8c1a8e4432d196bbeb7
bolstad <bolstad@bc3139a8-67e5-0310-9ffc-ced21a209358>
2005-11-12 00:55:28 +0000

    update bg.correct.gcrma so it wraps the new bg.adjust.gcrma function in gcrma package.
    Update and make more current various email and web addresses.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affyPLM@14816 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bg.correct.gcrma.R
+++ R/bg.correct.gcrma.R
@@ -1,30 +1,20 @@
-bg.correct.gcrma<- function(object,affinity.info=NULL,
-                  type=c("fullmodel","affinities","mm","constant"),
-                  k=6*fast+0.5*(1-fast),stretch=1.15*fast+1*(1-fast),
-                  correction=1,rho=0.7,
-                  optical.correct=TRUE,verbose=TRUE,fast=TRUE){
+bg.correct.gcrma<-  function(object,affinity.info=NULL,
+                            affinity.source=c("reference","local"),
+                            NCprobe=NULL,
+                            type=c("fullmodel","affinities","mm","constant"),
+                            k=6*fast+0.5*(1-fast),stretch=1.15*fast+1*(1-fast),correction=1,
+                            GSB.adjust=TRUE,rho=0.7,
+                            optical.correct=TRUE,verbose=TRUE,fast=TRUE)
+{
 
-  type <- match.arg(type)
+  bg.adjust.gcrma(object,
+                            affinity.info=affinity.info,
+                            affinity.source=affinity.source, NCprobe=NCprobe,
+                            type=type,k=k,stretch=stretch,correction=correction,
+                            GSB.adjust=GSB.adjust,rho=rho,
+                            optical.correct=optical.correct,verbose=verbose,fast=fast)
 
 
-  pmonly <- (type=="affinities"|type=="constant")
-  needaff <- (type=="fullmodel"|type=="affinities")
-  if( needaff & is.null(affinity.info)){
-    if(verbose) cat("Computing affinities")
-    affinity.info <- compute.affinities(cdfName(object),verbose=verbose)
-    if(verbose) cat("Done.\n")
-  }
-
-  if(optical.correct)
-    object <- bg.adjust.optical(object,verbose=verbose)
-
-  pm(object) <- gcrma.engine(pms=pm(object),
-                             mms=mm(object),
-                             pm.affinities=pm(affinity.info),
-                             mm.affinities=mm(affinity.info),
-                             type=type,k=k,
-                             stretch=stretch,
-                             correction=correction,rho=rho,
-                             verbose=verbose,fast=fast)
-  object
+  
+  
 }
