7a2b1f1a4fd6a07aa14bded6fd4265aa334559e6
bolstad <bolstad@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-06-05 20:30:33 +0000

    functions that separate out two parts of the quantile normalization algorithm.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/affyPLM@18318 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/quantile_extensions.R
+++ R/quantile_extensions.R
@@ -37,3 +37,41 @@ normalize.AffyBatch.quantiles.probeset <- function(abatch,type=c("separate","pmo
 }
 
 
+
+
+
+
+normalize.quantiles.determine.target <- function(x,target.length=NULL){
+
+  if (!is.matrix(x)){
+    stop("This function expects supplied argument to be matrix")
+  }
+  if (!is.numeric(x)){
+    stop("Supplied argument should be a numeric matrix")
+  }
+  rows <- dim(x)[1]
+  cols <- dim(x)[2]
+
+  if (is.integer(x)){
+    x <- matrix(as.double(x), rows, cols)
+  }
+  
+  if (is.null(target.length)){
+    target.length <- rows
+  }
+  
+  if (target.length <= 0){
+    stop("Need positive length for target.length")
+  }
+
+
+  return(.Call("R_qnorm_determine_target",x,target.length,PACKAGE="affyPLM"))
+  
+
+}
+
+
+
+normalize.quantiles.use.target <- function(x,target,copy=TRUE){
+  return(.Call("R_qnorm_using_target",x,target,copy,PACKAGE="affyPLM"))
+}
