615a8bd1a393b93a6fd0709e1232beab3aea5de2
Ben Bolstad <bmb@bmbolstad.com>
2014-09-03 07:27:55 -0700

    fix conflicting declaration

--- src/PLM_biweight.h
+++ src/PLM_biweight.h
@@ -4,7 +4,6 @@
 #include "threestep_summary_methods_param.h"
 
 void TukeyBiweight_threestep(double *data, int rows, int cols, int *cur_rows, double *results, int nprobes, double *resultsSE, summary_plist *summary_param);
-double Tukey_Biweight(double *x, int length);
 void TukeyBiweight_PLM(double *data, int rows, int cols, int *cur_rows, double *results, int nprobes, double *resultsSE, double *residuals, summary_plist *summary_param);
 
 
